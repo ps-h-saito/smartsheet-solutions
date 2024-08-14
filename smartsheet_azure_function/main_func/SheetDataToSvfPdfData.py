0ecd83a098c8fb3e02264da958ad7b6f98efb8d6
Mitsuhiro-fujita <m-fujita@powersolutions.co.jp>
2024-08-14 21:08:40 +0900

    SVFCloud対応９

--- smartsheet_azure_function/main_func/SheetDataToSvfPdfData.py
+++ smartsheet_azure_function/main_func/SheetDataToSvfPdfData.py
@@ -1,8 +1,10 @@
-123
-
 import smartsheet
 import logging
 import smartsheet.token
 import main_func as tr
 from azure.storage.blob import BlobServiceClient
 from datetime import datetime as dt
+
+### 定義 ###
+PDF_NAME_CREATE_ITEM_LIST = ["伝票ID","顧客社名","書類区分"] # PDFファイル名称命名規約用項目名
+
